95c65fb2962ff6c94f4a584a4eacd2be9625cadd
Kevin <kevinpogi00018@gmail.com>
2025-08-20 12:50:43 +0800

    fix grid rows

--- src/Sections/Featured.jsx
+++ src/Sections/Featured.jsx
@@ -9,8 +9,8 @@ const Featured = () => {
       iconClass="ri-gallery-view-2"
       iconClass2="ri-edit-2-line"
     >
-      <div className="grid grid-cols-1  grid-rows-[400px_1fr] md:grid-rows-[220px_1fr] mb-4 place-items-center gap-2 w-full min-h-[20rem] md:min-h-[10rem] lg:grid-cols-2 lg:grid-rows-1">
-        <div className="grid grid-rows-[215px_1fr] md:grid-rows-1 md:grid-cols-[1fr_400px] lg:grid-cols-1 lg:grid-rows-[1fr_80px] ">
+      <div className="grid  grid-rows-[auto_auto] mb-4 place-items-center gap-2 w-full min-h-[20rem] lg:grid-cols-2 lg:grid-rows-1">
+        <div className="grid grid-rows-[auto_auto] md:grid-rows-1 md:grid-cols-[1fr_400px] lg:grid-cols-1 lg:grid-rows-[1fr_80px] ">
           <div className="w-full mb-auto aspect-[16/9] rounded-lg px-2">
             <video
               src="/tryvideo.mp4"
@@ -22,8 +22,8 @@ const Featured = () => {
               className="w-full h-full object-cover rounded-lg shadow-darkShadow"
             />
           </div>
-          <div className="w-full flex h-full ml-2">
-          <p className="mx-2 text-[.9rem] text-white font-sans font-medium">
+          <div className="w-full flex jusstify-center h-full p-3 md:pt-0 ">
+          <p className="text-[.9rem] text-white font-sans font-medium">
             Our little space for adventures on two wheels 🚴‍♂️❤️. Here we share
             our road trips, favorite routes, hidden spots, and memories from our
             journeys together. It’s mainly for us to keep, but anyone is welcome
