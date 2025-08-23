424b7edfc59b9809b873b975e527c6517cb1843c
Kevin <kevinpogi00018@gmail.com>
2025-08-23 11:58:10 +0800

    Try to fix main height again

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
 
   return (
     <>
-    <main className="w-full min-h-screen grid grid-rows-[auto_auto] lg:grid-rows-1 lg:grid-cols-[1fr_1fr] lg:p-4 lg:gap-4">
+    <main className="w-full min-h-screen grid grid-rows-[auto_auto] md:h-auto lg:grid-rows-1 lg:grid-cols-[1fr_1fr] lg:p-4 lg:gap-4">
       <div className="w-full min-h-screen flex flex-col md:min-h-[300px]">
         <Home/>
         <Featured/>
       </div>
-      <div className="w-full h-auto flex flex-col">
+      <div className="w-full h-auto flex flex-col justify-start">
         <Gallery/>
       </div>
       </main>
