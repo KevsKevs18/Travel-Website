94ee5020775d3299b0206d51e86bf9f5a1f9cfe9
Kevin <kevinpogi00018@gmail.com>
2025-08-23 12:04:54 +0800

    Try again 5x

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Wrapper from "./Components/Wrapper"
+
 import Home from "./Sections/Home"
 import Featured from "./Sections/Featured"
 import Gallery from "./Sections/Gallery"
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-    <main className="w-full min-h-screen grid grid-rows-[auto_auto] md:h-auto lg:grid-rows-1 lg:grid-cols-[1fr_1fr] lg:p-4 lg:gap-4">
-      <div className="w-full min-h-screen flex flex-col md:min-h-[300px]">
+    <main className="w-full min-h-screen grid grid-rows-[auto_auto] md:min-h-[300px] lg:grid-rows-1 lg:grid-cols-[1fr_1fr] lg:p-4 lg:gap-4">
+      <div className="w-full h-auto flex flex-col">
         <Home/>
         <Featured/>
       </div>
