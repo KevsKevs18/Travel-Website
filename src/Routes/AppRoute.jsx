0772629bfaf912dadf6fb02c01915aaa40fc45b6
Kevin <kevinpogi00018@gmail.com>
2025-08-24 09:19:27 +0800

    Fixing vercel issue

--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./Pages/Homepage.jsx";
-import Beach from "./Pages/Beach.jsx";
-import Cities from "./Pages/Cities.jsx";
+import Homepage from "../Pages/Homepage.jsx";
+import Beach from "../Pages/Beach.jsx";
+import Cities from "../Pages/Cities.jsx";
 
 
 const AppRoute = () => {
